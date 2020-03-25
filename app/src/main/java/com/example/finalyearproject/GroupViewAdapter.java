86042d2e019557d4af4ff62aa9819e8da2e9e167
Cormac White <Cormac.White@evros.ie>
2020-03-25 17:20:38 +0000

    Can now view all members in the group

---
+++ app/src/main/java/com/example/finalyearproject/GroupViewAdapter.java
@@ -0,0 +1,94 @@
+package com.example.finalyearproject;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.finalyearproject.ui.tools.ToolsFragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupViewAdapter extends RecyclerView.Adapter<GroupViewAdapter.MyViewHolder> {
+    private ArrayList<Member> values;
+    public static final String MESSAGE_KEY1 ="text";
+    public static final String MESSAGE_KEY2 ="position";
+    // Provide a reference to the views for each data item
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtHeader;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            txtHeader = (TextView) itemView.findViewById(R.id.accountType);
+        }
+    }
+
+    // Provide the dataset to the Adapter
+    public GroupViewAdapter(ArrayList<Member> myDataset) {
+        values = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.account_layout, parent, false);
+//        View v = inflater.inflate(R.layout.account_layout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final Member type = values.get(position);
+        holder.txtHeader.setText(type.getName());
+
+        holder.txtHeader.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(v.getContext() ,type.getId(), Toast.LENGTH_SHORT ).show();
+                //addItem(position);
+                // call activity to pass the item position
+
+            }
+
+            public void addItem(int position, Member item){
+                values.add(position, item); //values is the ArrayList
+                notifyItemInserted(position);
+            }
+
+            public void remove(int position){
+                values.remove(position);
+                notifyItemRemoved(position);
+            }
+
+            public void update(Member newItem, int position){
+                values.set(position, newItem);
+                notifyItemChanged(position);
+            }
+
+        });
+
+    }
+
+    // Return the size of your dataset
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+}
+
--- app/src/main/java/com/example/finalyearproject/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/finalyearproject/ui/gallery/GalleryFragment.java
@@ -5,31 +5,104 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.finalyearproject.AccountAdapter;
+import com.example.finalyearproject.GroupViewAdapter;
+import com.example.finalyearproject.Leader;
+import com.example.finalyearproject.Member;
+import com.example.finalyearproject.Parent;
 import com.example.finalyearproject.R;
+import com.example.finalyearproject.ViewEvent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static androidx.recyclerview.widget.LinearLayoutManager.VERTICAL;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference mRef;
+
+    String email;
+    String name;
+    String group;
+    String id;
+    String DOM;
+    String DOB;
+    String notes;
+    String token;
+
+    ArrayList<Member> myDataset = new ArrayList<>();
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        recyclerView = (RecyclerView) root.findViewById(R.id.groupView);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setHasFixedSize(true);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("Person").child("Member");
+        mRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //name = ds.getValue(Member.class).getName();
+                    name = ds.getValue(Member.class).getName();
+                    email = ds.getValue(Member.class).getEmail();
+                    group = ds.getValue(Member.class).getGroup();
+                    id = ds.getValue(Member.class).getId();
+                    DOM = ds.getValue(Member.class).getMemDom();
+                    DOB = ds.getValue(Member.class).getMemDob();
+                    notes = ds.getValue(Member.class).getNotes();
+                    token = ds.getValue(Member.class).getFcmToken();
+
+                    Member m = new Member(id, name, email,group,DOB, DOM, notes, token);
+                    myDataset.add(m);
+                }
+
+                setAdapter();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
+
         return root;
+    }
+
+    public void setAdapter() {
+        LinearLayoutManager myLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(myLayoutManager);
+        GroupViewAdapter mAdapter = new GroupViewAdapter(myDataset);
+        recyclerView.addItemDecoration(new
+
+                DividerItemDecoration(getActivity(), VERTICAL));
+        recyclerView.setAdapter(mAdapter);
     }
 }
